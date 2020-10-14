5b6747247618d57f987d0d18f40df19949917a27
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-10-14 16:29:35 +0200

    Scripts cleaned up and organized

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// Listens to 'keydown' events on the window
-// Event = object containing data
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // Attribute selector using an ES6 template string
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; // Stops the function from executing if there is no audio associated with the key
   audio.currentTime = 0; // Rewinds file - so if the button is pressed repeatedly it starts playing on every press
   audio.play();
   key.classList.add('playing'); // For the animation class
-});
+}
 
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return; // Skips it if iT's not a transform
@@ -16,3 +14,6 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition)); // Listens to every single key - when transition ends, removes animation effect
+
+// Listens to 'keydown' events on the window
+window.addEventListener('keydown', playSound)
